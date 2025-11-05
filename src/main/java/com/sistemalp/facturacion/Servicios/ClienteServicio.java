940e79361e33e244e4496a1e8f3fe6aab69b934c
LFPINOSC <66181560+LFPINOSC@users.noreply.github.com>
2025-11-05 15:34:02 -0500

    guardar

--- src/main/java/com/sistemalp/facturacion/Servicios/ClienteServicio.java
+++ src/main/java/com/sistemalp/facturacion/Servicios/ClienteServicio.java
@@ -3,8 +3,6 @@ package com.sistemalp.facturacion.Servicios;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import javax.management.RuntimeErrorException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
